6db0f6bede07d2d4d27c23e1078762273172a79a
AndrewLondon <andrewmart@mail.com>
2025-01-08 15:27:27 +0000

    Arbitrage updated

--- arbitrage-scanner/main.go
+++ arbitrage-scanner/main.go
@@ -1,9 +1,20 @@
 package main
 
+import "os"
+
 // TickerData struct matches the JSON response from the binance-connector
 type TickerData struct {
 	Symbol     string  `json:"symbol"`
 	Bid        float64 `json:"bid"`
 	Ask        float64 `json:"ask"`
 	LastUpdate string  `json:"last_update"`
+}
+
+func main() {
+	// Read environment variables
+	connectorURL := os.Getenv("CONNECTOR_URL")
+	if connectorURL == "" {
+		connectorURL = "http://binance-connector:8001/latest-price" // Default value
+	}
+
 }
