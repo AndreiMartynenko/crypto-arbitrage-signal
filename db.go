7c6f8c35627408110eb40d32fe02053dc23fb98c
AndrewLondon <andrewmart@mail.com>
2025-01-17 23:51:33 +0000

    Added db

--- db.go
+++ db.go
@@ -2,3 +2,40 @@ package main
 
 //Create a database connection
 //Postgres
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq" // PostgreSQL driver
+	"log"
+	"os"
+)
+
+// Database configuration
+var db *sql.DB
+
+func initDB() {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Database connection test failed: %v", err)
+	}
+
+	log.Println("Connected to the database successfully!")
+}
+
